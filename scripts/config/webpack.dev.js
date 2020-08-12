e5f87e89f024e28fc6a3da7c5ef369e0757604c1
vortesnail <1091331061@qq.com>
2020-08-12 15:28:03 +0800

    chore: initialize all dev enviroment

---
+++ scripts/config/webpack.dev.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const { SERVER_HOST, SERVER_PORT } = require('../constants')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    host: SERVER_HOST,
+    port: SERVER_PORT, // 默认是8080
+    stats: 'errors-only', // 终端仅打印 error
+    clientLogLevel: 'silent', // 日志等级
+    compress: true, // 是否启用 gzip 压缩
+    open: true, // 打开默认浏览器
+    hot: true, // 热更新
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+})
