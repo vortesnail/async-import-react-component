2d8ef70717fb924fb26919e970e062838ef10473
vortesnail <1091331061@qq.com>
2020-08-18 17:21:51 +0800

    chore: add rollup config terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
@@ -21,7 +22,6 @@ export default {
   ],
   watch: { include: 'src/**' },
   plugins: [
-    nodeResolve(),
     babel({
       babelHelpers: 'bundled',
       extensions,
@@ -29,5 +29,7 @@ export default {
       exclude: /node_modules/,
     }),
     commonjs({ include: /node_modules/ }),
+    nodeResolve(),
+    terser(),
   ],
 }
